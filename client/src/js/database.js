5d767b0a09a12b6a988f0779e597197e70113b48
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-04-22 21:40:38 +1000

    Finished getDb database and fixed getDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,12 +17,21 @@ export const putDb = async (content) => {
   console.log('putting to the database');
   const jateDb = await openDB('jate', 1);
   const txn = jateDb.transaction('jate', 'readwrite');
-  const dbStore = tx.objectStore('jate');
+  const dbStore = txn.objectStore('jate');
   const dbReq = dbStore.put({ id: 1, value: content });
   const res = await dbReq;
 }
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const txn = jateDb.transaction('jate', 'readonly');
+  const dbStore = txn.objectStore('jate');
+  const dbReq = dbStore.get(1);
+  const res = await request;
+
+  return res?.value;
+}
 
 initdb();
